02431794cd9618900d4ea05965eab17d1dce6faa
busradogru <hbdogru@gmail.com>
2016-01-05 19:12:41 +0200

    note.m dosyası düzenlendi

--- note.m
+++ note.m
@@ -1,20 +1,17 @@
-function [topla,t]=note(frekans,vurus) 
-Fs=8192;
-topla=0;
-a=1;
-harmonik={1 0.8 0.4 0.1};
-t=0:1/Fs:vurus-1/Fs;
-for h=1:length(harmonik)
-   x=harmonik{h}*sin(2*pi*frekans*t*a); 
-   topla=topla+x;
-   a=a+1;
-end
-   z1=linspace(0,1.5,length(t)/4);
-   z2=linspace(1.5,1,length(t)/8);
-   z3=linspace(1,1,length(t)/2);
-   z4=linspace(1,0,length(t)/8);
-   zarf=[z1 z2 z3 z4];
-   x=x+zarf;
-   
-   
-end
+function[x,t]=note(frekans,vurus)% genliði frekansý ve vuruþ deðeri verilen notanýn sinüs sinyalini çizdiren fonksiyon
+    fs=8192;%örnekleme sayýsý deðiþkene tanýmlandý
+    t=0:1/fs:vurus-(1/fs); %sinyalin periyodu tanýmlandý
+       a=length(t);%periyod boyutu a deðiþkenine atandý
+    hk=[1 0.8 0.4 0.1]; %harmonik katsayýlaru diziye atandý
+        b=length(hk); %hk dizisinin boyutu b deðiþkenine atandý
+        x=zeros(1,a); %periyot deðerinin boyutu kadar sýfýr deðerinden oluþan bir x matrisý tanýmlandý
+        for i=1:b %1 den hk dizisi boyutuna kadar bir döngü oluþturuldu
+            x=x+(hk(i)*sin(2*pi*(i*frekans)*t)); %dýþarýdan alýnan deðerlere göre oluþturulan sinus sinyalinin harmoniði oluþturuldu
+        end
+A=linspace(0,1.5,a/4);%genliði 1,5'a kadar çýkan ve periyodun 1/4'u kadar aralýkta vektör oluþturur
+B=linspace(1.5,1,a/8);%genliði 1,5 dan 1'e kadar inen ve periyodun 1/8'i kadar aralýkta vektör oluþturur
+C=linspace(1,1,a/2);%genliði sabitolan ve periyodun 1/2'si kadar aralýkta vektör oluþturur
+D=linspace(1,0,a/8);%genliði 1 den 0'e kadar inen ve periyodun 1/8'i kadar aralýkta vektör oluþturur
+ zarf=[A B C D];%oluþturulan tüm vektörler bir dizide peþpeþe sýralanýr
+ x=x.*zarf; %x dizisinin her elemani zarf dizisinin her elemani tek tek çarpýlara tekrar x dizisine atanýr yani x sinyali zarflanýr.
+ 
