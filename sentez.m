af001316be0e4129c1449a7a95f99ad507ba4326
busradogru <hbdogru@gmail.com>
2016-01-05 19:13:01 +0200

    sentez.m dosyası düzenlendi

--- sentez.m
+++ sentez.m
@@ -1,31 +1,44 @@
-Fs=8500; %ornekleme frekansý deger atamasý yapýldý.
-gecikme=round(Fs/100); %echo icin gecikme suresi tanýmlandý.
-notalar=[]; %notalar matrisine ilk deger atamasý yapýldý.
-duraklama=[]; %Fs/10000; %duraklama suresi degeri atandý.
-oktdegis=3; %oktav degerlerini degistiren degisken[-3,+3].
-dosya=fopen('notalar.txt','r'); %text dosyasýný acar,'r':okumak için.
-[nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %text dosyasý okundu ve , ile ayrýlmýs degiskenler yeni matrislere atandý.
-fclose(dosya); %text dosyasý kapatýldý.
-%% Program icinde oktav degisimi kosulu
-if oktdegis~=0 %oktdegis degiskeninde degisiklik yapýlmasý durumunda islenecek sart. 
-    for j=1:length(oktav) %text'ten cekilen oktav degerleri dongu icerisinde degistirilecek.
-        oktav(j)=oktav(j)+oktdegis; 
-    end
-end
-%% Notalar matrisi icinde notalarýn sinyallerini olusturma
-for i=1:length(nota)
-    frekans=frek(nota{i},oktav(i)); %frek fonsiyonu cagýrýlarak i. notanýn frekans degeri hesaplandý. 
-    [sindalga,t]=note(frekans,str2num(olcu{i})); %note fonksiyonu cagýrýlarak i.notanýn sinus sinyali cizildi.
-    notalar=[notalar sindalga duraklama]; %notalarýn sinyalleri arasýna bosluk eklenerek notalar matrisinde birlestirildi.
-end  
-%% Notalar matrisine echo ekleme
-for i=1:length(notalar)
-    if (i+gecikme)<=length(notalar) %gecikme notalar uzunlugunu asmadýgý surece islenecek sart.
-        notalar(i+gecikme)=notalar(i+gecikme)+0.3*notalar(i); %Fs, 10'da biri süresince geciktirilip(gecikme)
-    end                                               %++genliði %30 oranýnda düþürülerek kendisi ile toplandý.
-end
-%% Notalar matrisi sinyallerinin tepe degeri 1'e normalize edildi.
-notalar=notalar/max(abs(notalar));
-%% Notalar matrisinin grafigi cizdirilip, ses olarak calýndý
-plot(notalar)
-sound(notalar,Fs);
+%% 
+
+    [nota,b,c]=textread('notalar.txt','%s %s %s'); %notalar.txt dosyasý okundu ve içerisindeki her sütun bir deðere atandý.
+    oktavdegeri=1; %oktav degeri.
+    fs=8192;%örnekleme sayýsý deðiþkene atandý.
+    ds=round(fs/100); %durma süresi belirlendi.
+    go=round(fs/10); %gecikme süresi belirlendi.
+
+    notalar=zeros(1,fs); %bir dizi oluþturuldu.
+    a=zeros(1,length(notalar)); %notalar matrisi boyutlarýnda bir dizi oluþturuldu.
+    durak=zeros(1,ds); %durma süresi boyutunda bir dizi oluþturuldu.
+    oktav=str2num(cell2mat(b)); %deðiþkenler double deðere dönüþtürüldü.
+  %%
+
+for i=1:(length(nota)) %oluþturulan döngü nota dizisi boyu kadar. 
+       vurus((i),1)=str2num(cell2mat(c(i,1))); %dosyadan cell olarak okunan deðiþkenler double deðere dönüþtürüldü.
+       frekans((i),1)=frek(nota((i),1),oktav((i),1),oktavdegeri); %önceden oluþturulmuþ frek fonsiyonu çaðrýldý.
+       [x,t]=note(frekans((i),1),vurus((i),1)); %daha önceden olýþturulmuþ note fonksiyonu çaðýrýldý.
+       notalar=[notalar durak x]; %durak dizisi de eklenerek note fonksiyonunun döndürdüðü sinyal deðeriyle birleþtirildi.
+end           
+   plot(notalar)
+   title('Zarflanan nota sinyalleri duraklama süresi ile birlikte peþpeþe eklendi')
+ %%
+   notalarecho=zeros(1,(length(notalar))); %notalar matrisi boyutlarýnda sýfýrdan olusan bir dizi oluþturuldu
+ for i=1:(length(notalar)) %döngü notalar dizisi boyu kadar oluþturuldu.    
+     f=(go+i); %gerekli olan gecikme süresi döngüye sokuldu.
+     if f<(length(notalar)) %gecikme süresi notalar matrisinin boyutunu geçmesin diye þart saðlandý
+     notalarecho(i)=notalar(i)*0.3+notalar(f); %notalar matrisinin herbir deðerinin %30'u alýnýp geçikme süresi kadar kaydýrýlmýþtýr
+     else 
+      notalarecho(i)=notalar(i)*0.3+0;
+     end
+    
+ end 
+ plot(notalarecho)
+     title('Echolanan Nota Sinyalleri')
+
+  normalizenotalarecho=zeros(1,(length(notalar)));%deðiþken tanýmlanmýþtýr
+  normalizenotalarecho=notalarecho/max(abs(notalarecho));%tepe deðerleri 1'e normalize edilmiþtir.
+
+  sound(normalizenotalarecho) %sinyal çaldýrýlýyor.
+    plot(normalizenotalarecho)
+    title('Nomalize Edilmiþ Nota Sinyalleri')
+
+    
