aa820749dd5e738fdc1aa6315ef6100c160d9b01
busradogru <hbdogru@gmail.com>
2016-01-05 19:12:01 +0200

    frek.m dosyası düzenlendi

--- frek.m
+++ frek.m
@@ -1,27 +1,33 @@
-%% Fonksiyon tanýmý
-function ff=frek(nota,oktav) %girilen nota ve oktav degerine gore frekansi hesaplayan fonksiyon tanimi
-%% Deger atamalarý
-notalar={'do' 'dod' 're' 'red' 'mi' 'fa' 'fad' 'sol' 'sold' 'la' 'lad' 'si' 'sus'};%Lad=La#,Sib;Dod=Do#,Reb;Red=Re#,Mib;Fad=Fa#,Solb;Sold=Sol#,Lab
-referans=16.35;    %referans degeri atandý.
-p=length(notalar); %notalar dizisinin eleman sayisi 
-%% Oktav degerinin girilmemesi durumu
-if nargin<2        %girilen arguman sayýsýnýn 2'den az olma durumu(oktav degeri girilmemis ise).
-    oktav=4;       %oktav'a 4 degerini ata.
-end
-%% Frekans degerinin hesaplanmasý
-for okt=0:8 %okt(oktav) degerini 0'dan 8(dahil)'e kadar artýrma islemi
-    if okt==oktav  %girilen oktav degerinin okt degeriyle esitlenmesi halinde
-        for j=1:p  %++dizi eleman indisinin artýrýmý    
-            if size(nota)==size(notalar{j}) %girilen ile dizideki notalarin uzunluklari esit ise
-                if nota==notalar{j}         %++girilen notanin dizideki karsiliginin(indisinin) bulunmasi 
-                    if j==p                 %++bulunan nota 'sus' ise 
-                        ff=0;               %++frekansý degerini 0 ata.
-                    else                    %'sus' dýsýndaki notalar icin
-                        ff=2^okt*(2^((j-1)/12)*referans); %++oktav ve nota indisine bagli olarak
-                    end                                   %++frekansi hesaplayan formul
-                end                                   
-            end    
-        end
+function [frekans]=frek(nota,oktav,oktavdegeri)%frekans döndürmektedir.
+
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','Sus'}; %notolardan oluþan bir dizi yazdýk.
+n=strcmp(notalar,nota); %Dizenin içine aktarý,indekside 1 artýrýr.
+m=find(n,1);%n dizisinde 1 deðerinin indeksini döndürür.
+        if (nargin==0) %fonsiyona hiç deðer gönderilmemiþ ise
+                 disp ('veri giriþi yapýnýz');%deðer girilmesini talep eder
+        elseif (nargin==1)%tek deðer gönderilmiþ ise
+                 if (m==13)%deðerin Sus notasý olup olmadýðýný konrol eder
+                    frekans=0;%sus deðeri ise frekansý 0 döndürür
+                else %deðilse
+                    frekans=16.35*(2^(4))*(2^((m-1)/12));%fonksiyona gönderilen tek deðeri nota kabul eder ve oktavý 4 oktavdeðerini 0 kabul ederek frekans hesaplar
+                end%sus notasý kontrol iþlemini bitirir
+        elseif (nargin==2) %eðer iki deðer gönderilmiþ ise
+                 if (m==13)%ve nota deðeri Sus ise
+                    frekans=0;%frekansý 0 döndürür
+                 else%deðil ise
+                    frekans=16.35*(2^(oktav))*(2^((m-1)/12)); %oktav fonksiyonuna göre frekans üretir
+                 end
+        else %eðer 2 den fazla deðer gönderilmiþ ise
+                 if (m==13)%ve nota deðeri Sus ise
+                     frekans=0;%frekansý 0 döndürür
+                 else%deðil ise
+                     frekans=16.35*(2^(oktav+oktavdegeri))*(2^((m-1)/12)); %gönderilen nota oktav ve oktav deðerine göre frekans hesaplamasý yapar
+                 end
+
+
     end
-end
-end
+
+end 
+   
+
+
